62bd06a546df3681230618f5972477e86259ef7a
unknown <judithportocarrero99@gmail.com>
2020-09-29 04:19:44 -0500

    Updates

---
+++ precache-manifest.bebd6006be87206262aa2aaedc830fee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a8d0d26a3b39fdb5c83f1273f6e7bb5",
+    "url": "/carrodecompras/index.html"
+  },
+  {
+    "revision": "0149465b15b58efecad1",
+    "url": "/carrodecompras/static/css/main.9b579450.chunk.css"
+  },
+  {
+    "revision": "12a1d99d425a45b9361c",
+    "url": "/carrodecompras/static/js/2.81ae36d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/carrodecompras/static/js/2.81ae36d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0149465b15b58efecad1",
+    "url": "/carrodecompras/static/js/main.38eaa36d.chunk.js"
+  },
+  {
+    "revision": "eb152b4950833eec4ba5",
+    "url": "/carrodecompras/static/js/runtime-main.4dfc7079.js"
+  }
+]);
